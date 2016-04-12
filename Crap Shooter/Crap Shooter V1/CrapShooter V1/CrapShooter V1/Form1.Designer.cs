05ecabbad0b27092e32d0a3731887ed58b71c48d
Bronson Brown <bronson1998@live.com>
2016-04-11 22:48:31 -0400

    Crap Shooter was added
    first push

---
+++ Crap Shooter/Crap Shooter V1/CrapShooter V1/CrapShooter V1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CrapShooter_V1
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.laserCannon = new System.Windows.Forms.PictureBox();
+            this.lazerBeam = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.laserCannon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lazerBeam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // laserCannon
+            // 
+            this.laserCannon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("laserCannon.BackgroundImage")));
+            this.laserCannon.Image = ((System.Drawing.Image)(resources.GetObject("laserCannon.Image")));
+            this.laserCannon.Location = new System.Drawing.Point(322, 293);
+            this.laserCannon.Name = "laserCannon";
+            this.laserCannon.Size = new System.Drawing.Size(60, 60);
+            this.laserCannon.TabIndex = 0;
+            this.laserCannon.TabStop = false;
+            // 
+            // lazerBeam
+            // 
+            this.lazerBeam.Image = ((System.Drawing.Image)(resources.GetObject("lazerBeam.Image")));
+            this.lazerBeam.Location = new System.Drawing.Point(351, 293);
+            this.lazerBeam.Name = "lazerBeam";
+            this.lazerBeam.Size = new System.Drawing.Size(3, 10);
+            this.lazerBeam.TabIndex = 1;
+            this.lazerBeam.TabStop = false;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(684, 396);
+            this.Controls.Add(this.lazerBeam);
+            this.Controls.Add(this.laserCannon);
+            this.Name = "mainWindow";
+            this.Text = "BCB\'s Lazer Cannon";
+            this.Load += new System.EventHandler(this.mainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.laserCannon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lazerBeam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox laserCannon;
+        private System.Windows.Forms.PictureBox lazerBeam;
+    }
+}
+
---
+++ Crap Shooter/Crap Shooter V1/CrapShooter V1/CrapShooter V1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CrapShooter_V1
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+            // Intializes this componet not really sure of its purpose
+            // Ask Chris about this below
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+        // Things to load on start
+        private void mainWindow_Load(object sender, EventArgs e)
+        {
+            lazerBeam.Visible = false;
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Shortens the location to just one character varaiable
+            // To be fed into parameters later
+            int x = laserCannon.Location.X;
+            int y = laserCannon.Location.Y;
+            int xy = 293;
+            
+            if (e.KeyCode == Keys.Space)
+            {
+                lazerBeam.Visible = true;
+                int xyz = x;
+                for (int i = 1; i <= 300; i++)
+                {
+                    xy += 1;
+                    lazerBeam.Location = new Point(xyz, xy);
+                    for (double n = 1; n <= 999; n++) ;
+                }
+                
+            }
+            // Checks for Pressed Keys
+            // Checks right key
+            if (e.KeyCode == Keys.Right)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    // Adds a field limit
+                    if (x >= 625)
+                    {
+                        // Prevents element from moving OOF
+                        laserCannon.Location = new Point(x, y);
+                    }
+                    else
+                    {
+                        x += 1;
+                        laserCannon.Location = new Point(x, y);
+                        // Adds frames for an animated effect
+                        for (double n = 1; n <= 400; n++) ;
+                    }
+                }
+            }
+            //Checks left key
+            else if (e.KeyCode == Keys.Left)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    if (x <= 0)
+                    {
+                        laserCannon.Location = new Point(x, y);
+                    }
+                    else
+                    {
+                        x -= 1;
+                        laserCannon.Location = new Point(x, y);
+                        for (double n = 1; n <= 400; n++) ;
+                    }
+                }
+            }
+            // Will use to make longer faster slides left
+            else if (e.KeyCode == Keys.A)
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    if (x <= 0)
+                    {
+                        laserCannon.Location = new Point(x, y);
+                    }
+                    else {
+                        x -= 1;
+                        laserCannon.Location = new Point(x, y);
+                        for (int n = 1; n <= 400; n++) ;
+                    }
+                }
+            }
+
+            // Will use to make longer faster slides right
+            else if (e.KeyCode == Keys.D)
+            {
+                for (double i = 1; i <= 100; i++)
+                {
+                    if (x >= 625)
+                    {
+                        laserCannon.Location = new Point(x, y);
+                    }
+                    else
+                    {
+                        x += 1;
+                        laserCannon.Location = new Point(x, y);
+                        for (double n = 1; n <= 400; n++) ;
+                    }
+                }
+            }
+
+        }
+
+    }
+}
